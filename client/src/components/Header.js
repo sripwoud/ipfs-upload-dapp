3c0ac3b70d432fc076ca73f3218b05796875f6d1
6ry0u <6ry0u@tutanota.com>
2019-11-09 16:18:14 +0100

    Add form to load files and result message

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,11 +4,15 @@ import { Header, Icon } from 'semantic-ui-react'
 export default () => {
   const style = { marginTop: '20px' }
   return (
-    <Header as='h1' icon style={style}>
+    <Header as='h1' icon style={style} textAlign='center'>
       <Icon name='upload' />
         IPFS File upload
       <Header.Subheader>
-        Upload file to Interplanetary File System and store corresponding hash on the Ethereum blockchain
+        Upload files to the &nbsp;
+        <a href='https://ipfs.io/' target='-blank'>
+          Interplanetary File System
+        </a>
+         &nbsp;and store corresponding hash on the Ethereum blockchain
       </Header.Subheader>
     </Header>
   )
