fa7bc207430c11227c831bba7be27bac49c25d96
6ry0u <6ry0u@tutanota.com>
2019-11-11 23:13:12 +0100

    fix: Enable metamask provider

--- client/src/ethereum/web3.js
+++ client/src/ethereum/web3.js
@@ -1,17 +1,15 @@
 import Web3 from 'web3'
-require('dotenv').config()
 
 let web3
-if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+if (typeof window.ethereum !== 'undefined') {
   /* We are in the browser & Metamask is installed:
- Metamask has injected web3 in the browser
-(won't work with Server Side Rendering) */
-  web3 = new Web3(window.web3.currentProvider)
+  Ethereum user detected. You can now use the provider. */
+  window.ethereum.enable()
+  const provider = window.ethereum
+  web3 = new Web3(provider)
 } else {
   /* We are not in the browser (server side rendering)
   or user is not running Metamask */
-  // const infuraKey = fs.readFileSync('./.infuraKey').toString().trim()
-  console.log(process.env.INFURA_KEY)
   const provider = new Web3.providers.HttpProvider(`https://rinkeby.infura.io/${process.env.INFURA_KEY}`)
   web3 = new Web3(provider)
 }
