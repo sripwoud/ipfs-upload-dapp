f41835b070fec19b6126f05b95943cad74ba4564
6ry0u <6ry0u@tutanota.com>
2019-11-18 22:40:50 +0100

    Change type from bytes32 to string

--- client/src/ethereum/hash.js
+++ client/src/ethereum/hash.js
@@ -4,7 +4,7 @@ import { abi } from './hash.json'
 import { provider } from './ethers'
 
 export default new Contract(
-  '0x2652162Fa9A6A6CFA38b86a8b4A310990b1F65B0',
+  '0x174D16C6940Af5d13875045cC35312f8C618AD2F',
   abi,
   provider.getSigner(0)
 )
