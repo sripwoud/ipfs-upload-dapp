a02ba8022f0a8f7bcc6c2be221844eb2403bf084
sripwoud <me@sripwoud.xyz>
2019-11-20 23:00:02 +0100

    Unpack openzeppeling GSN starter kit

--- client/src/ipfs.js
+++
@@ -1,7 +0,0 @@
-import IPFS from 'ipfs-http-client'
-
-export default new IPFS({
-  host: 'ipfs.infura.io',
-  port: 5001,
-  protocol: 'https'
-})
--- ethereum/contract/Hash.vy
+++
@@ -1,5 +0,0 @@
-ipfsHash: public(string[50])
-
-@public
-def setHash(_hash: string[50]):
-  self.ipfsHash = _hash
